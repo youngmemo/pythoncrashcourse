4115d5ac3ea1b90ee2e34275e01dfc58c7aeea4a
Mehmet Eksi <mehmeteksi99@hotmail.com>
2022-01-20 13:41:27 +0100

    Pushed up python files

---
+++ buffet.py
@@ -0,0 +1,4 @@
+buffet_food = ("Kebab", "Pizza", "Falafel", "Suppe")
+
+for food in buffet_food:
+    print(food)
---
+++ dimensions.py
@@ -0,0 +1,9 @@
+dimensions = (200, 50)
+print("Original dimension")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
